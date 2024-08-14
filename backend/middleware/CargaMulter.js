e8983891c60d9f0d2ef6973ce53dfb5e9c3e883d
fixbala <smartineza_2@uqvirtual.edu.co>
2024-08-14 10:53:11 -0500

    Documentacion actualizada.

--- backend/middleware/CargaMulter.js
+++ backend/middleware/CargaMulter.js
@@ -1,31 +1,41 @@
-const multer = require('multer');
-const { extname, join } = require('path');
+const multer = require('multer'); // Importa el módulo multer para manejar la carga de archivos
+const { extname, join } = require('path'); // Importa funciones del módulo path para manejar rutas y extensiones de archivos
 
+// Ruta del directorio actual
 const CURRENT_DIR = __dirname;
+
+// Tipos MIME permitidos para la carga de archivos
 const MIMETYPES = ['image/jpeg', 'image/jpg', 'image/png'];
 
+/**
+ * Configuración de Multer para la carga de archivos.
+ * Define el almacenamiento en disco, el nombre de los archivos, los filtros de tipo de archivo y los límites de tamaño.
+ */
 const CargaArchivo = multer({
   storage: multer.diskStorage({ 
-    destination: join(CURRENT_DIR, '../cargas'),
+    destination: join(CURRENT_DIR, '../cargas'), // Define el directorio de destino para guardar los archivos cargados
     filename: (req, file, cb) => {
-      const extArchivo = extname(file.originalname);
-      const nombreArchivo = file.originalname.split(extArchivo)[0]
-        .split(extArchivo)[0]
-        .replace(/\s+/g, '_')
-        .toLowerCase();
+      const extArchivo = extname(file.originalname); // Obtiene la extensión del archivo
+      const nombreArchivo = file.originalname.split(extArchivo)[0] // Obtiene el nombre del archivo sin la extensión
+        .replace(/\s+/g, '_') // Reemplaza los espacios en blanco con guiones bajos
+        .toLowerCase(); // Convierte el nombre a minúsculas
 
-      cb(null, `${nombreArchivo}-${Date.now()}-${extArchivo}`);
+      cb(null, `${nombreArchivo}-${Date.now()}-${extArchivo}`); // Asigna el nombre final del archivo con una marca de tiempo
     },
   }),
   fileFilter: (req, file, cb) => {
-    if (MIMETYPES.includes(file.mimetype)) cb(null, true);
-    else {
-      cb(new Error('Tipo de archivo no permitido'), false);
+    if (MIMETYPES.includes(file.mimetype)) {
+      cb(null, true); // Acepta el archivo si el tipo MIME está permitido
+    } else {
+      cb(new Error('Tipo de archivo no permitido'), false); // Rechaza el archivo si el tipo MIME no está permitido
     }
   },
   limits: {
-    fieldSize: 10000000
+    fieldSize: 10000000 // Limita el tamaño del archivo a 10 MB
   }
 });
 
-module.exports = {CargaArchivo, CURRENT_DIR};
+/**
+ * Exporta la configuración de Multer y el directorio actual.
+ */
+module.exports = { CargaArchivo, CURRENT_DIR };
