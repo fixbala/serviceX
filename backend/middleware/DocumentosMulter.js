dde969cd8c9fe4f88b4efae03a312e3f140412e9
fixbala <smartineza_2@uqvirtual.edu.co>
2024-08-14 20:11:59 -0500

    inicio de la construccion del router para manejar el enrutamiento y recibir solicitudes.

--- backend/middleware/DocumentosMulter.js
+++ backend/middleware/DocumentosMulter.js
@@ -1,27 +1,22 @@
 const multer = require('multer'); // Importa el módulo multer para manejar la carga de archivos
 const { extname, join } = require('path'); // Importa funciones del módulo path para manejar rutas y extensiones de archivos
 
+
 // Ruta del directorio actual
-const CURRENT_DIR = __dirname;
-
-// Tipos MIME permitidos para la carga de documentos (actualmente está vacío, pero puedes habilitar los formatos comentados más adelante).
-const MIMETYPES = []; 
-
-/**
+  const CURRENT_DIR = __dirname;
+  /**
  * Ejemplo de configuración de tipos MIME permitidos:
- *
- * const MIMETYPES = [
- *   'application/pdf', // PDF
- *   'application/msword', // Doc
- *   'application/vnd.openxmlformats-officedocument.wordprocessingml.document', // Docx
- *   'application/vnd.ms-powerpoint', // PPT
- *   'application/vnd.openxmlformats-officedocument.presentationml.presentation', // PPTX
- *   'application/vnd.ms-excel', // XLS
- *   'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', // XLSX
- * ];
  */
-
-/**
+// Tipos MIME permitidos para la carga de documentos (actualmente está vacío, pero puedes habilitar los formatos comentados más adelante).
+  const MIMETYPES = ['application/pdf',          // PDF
+  'application/msword',       // Doc
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document', // Docx
+  'application/vnd.ms-powerpoint', // PPT
+  'application/vnd.openxmlformats-officedocument.presentationml.presentation', // PPTX
+  'application/vnd.ms-excel', // XLS
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', // XLSX 
+  ];
+/** 
  * Configuración de Multer para la carga de documentos.
  * Define el almacenamiento en disco, el nombre de los archivos, los filtros de tipo de archivo y los límites de tamaño.
  */
---
+++ backend/routers/auditoria.js
@@ -0,0 +1,36 @@
+// Importacion de express
+const express = require('express');
+const cors = require('cors');
+const pool = require('../database/db.js');
+
+const routerAuditoria= express.Router();
+
+routerAuditoria.use(express.json());
+routerAuditoria.use(cors());
+ 
+// get all
+routerAuditoria.get('/', async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM auditorias order by fecha desc, hora desc")
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ message: "Ha ocurrido un error" });
+  }
+});
+
+
+// get by id
+routerAuditoria.get('/:id_usuario', async (req, res) => {
+  try {
+ 
+    const {id_usuario} = req.params;
+    const result = await pool.query('SELECT * FROM auditorias  WHERE id_usuario = $1', [id_usuario])
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ message: "Ha ocurrido un error" });
+  }
+});
+
+
+
+module.exports = routerAuditoria;
