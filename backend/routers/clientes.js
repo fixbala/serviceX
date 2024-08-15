838a9a7a30f4f1fcf67ac66605de37f5e33f5e19
fixbala <smartineza_2@uqvirtual.edu.co>
2024-08-15 11:33:06 -0500

    Actualizacion de la  documentacion.

--- backend/routers/clientes.js
+++ backend/routers/clientes.js
@@ -1,28 +1,39 @@
-// Importacion de express
-const express = require('express');
-const cors = require('cors');
-const pool = require('../database/db.js');
-const { validationResult } = require('express-validator');
-const { auditar,convertirMayusculas } = require('../funciones/funciones.js');
+// Importación de módulos necesarios
+const express = require('express'); // Importa el framework Express para la creación de servidores y rutas
+const cors = require('cors'); // Importa el middleware CORS para permitir solicitudes desde diferentes orígenes
+const pool = require('../database/db.js'); // Importa la conexión a la base de datos
+const { validationResult } = require('express-validator'); // Importa express-validator para manejar validaciones
+const { auditar, convertirMayusculas } = require('../funciones/funciones.js'); // Importa funciones auxiliares de otro módulo
+const { validaClientes, validaidClientes } = require('../validaciones/ValidarClientes.js'); // Importa funciones de validación
 
-const { validaClientes, validaidClientes } = require('../validaciones/ValidarClientes.js');
-//const { auditar } = require('../funciones/funciones.js')
-
+// Crear un enrutador para las rutas de clientes
 const routerClientes = express.Router();
-routerClientes.use(express.json());
-routerClientes.use(cors());
 
+// Middlewares
+routerClientes.use(express.json()); // Middleware para parsear el cuerpo de las solicitudes como JSON
+routerClientes.use(cors()); // Middleware para habilitar CORS en las rutas
 
-//create
+/**
+ * POST /
+ * Crea un nuevo cliente si los datos son válidos y no hay conflictos en la base de datos.
+ *
+ * @route POST /
+ * @body {string} nombre - Nombre del cliente.
+ * @body {string} ubicacion - Ubicación del cliente.
+ * @body {string} telefono - Teléfono del cliente.
+ * @body {string} correo - Correo electrónico del cliente.
+ * @body {number} id_plan - ID del plan asociado al cliente.
+ * @body {string} estado_usuario - Estado del usuario asociado al cliente.
+ * @returns {Object} - Mensaje de éxito o error.
+ */
 routerClientes.post('/', validaClientes, async (req, res) => {
-
   const consulta = `
   WITH validaciones AS (
     SELECT
       EXISTS (SELECT 1 FROM planes WHERE id_plan = $5) AS existePlan,
       EXISTS (SELECT 1 FROM usuarios WHERE id_usuario = $6) AS existeUsuario,
       NOT EXISTS (SELECT 1 FROM clientes WHERE correo = $4) AS correoNoExiste
-      )
+    )
   INSERT INTO clientes (
     nombre, ubicacion, telefono, correo, id_plan, id_usuario, estado_usuario
   )
@@ -31,11 +42,10 @@ routerClientes.post('/', validaClientes, async (req, res) => {
   FROM validaciones
   WHERE existePlan = true AND existeUsuario = true AND correoNoExiste
   RETURNING *;
-`;
+  `;
+
   try {
-
     const { nombre, ubicacion, telefono, correo, id_plan, estado_usuario } = req.body;
-
     const operacion = req.method;
     const id_usuario = req.headers['id_usuario'];
 
@@ -47,8 +57,9 @@ routerClientes.post('/', validaClientes, async (req, res) => {
       const crearCliente = await pool.query(consulta, [
         camposMayus.nombre, ubicacion, telefono, correo, id_plan, id_usuario, estado_usuario
       ]);
+
       if (crearCliente.rows.length > 0) {
-        auditar(operacion,id_usuario);
+        auditar(operacion, id_usuario);
         return res.status(200).json({ mensaje: 'Cliente creado exitosamente' });
       } else {
         return res.status(400).json({ error: 'Error al crear el cliente' });
@@ -61,8 +72,20 @@ routerClientes.post('/', validaClientes, async (req, res) => {
   }
 });
 
-
-//modificar cliente
+/**
+ * PUT /:id_cliente
+ * Actualiza los datos de un cliente existente.
+ *
+ * @route PUT /:id_cliente
+ * @param {string} id_cliente - ID del cliente a actualizar.
+ * @body {string} nombre - Nombre del cliente.
+ * @body {string} ubicacion - Ubicación del cliente.
+ * @body {string} telefono - Teléfono del cliente.
+ * @body {string} correo - Correo electrónico del cliente.
+ * @body {number} id_plan - ID del plan asociado al cliente.
+ * @body {string} estado_usuario - Estado del usuario asociado al cliente.
+ * @returns {Object} - Mensaje de éxito o error.
+ */
 routerClientes.put('/:id_cliente', validaClientes, validaidClientes, async (req, res) => {
   const query = `
     UPDATE clientes
@@ -72,21 +95,19 @@ routerClientes.put('/:id_cliente', validaClientes, validaidClientes, async (req,
       telefono = $3,
       correo = $4,
       id_plan = $5,
-      id_usuario= $6,
+      id_usuario = $6,
       estado_usuario = $7
-      WHERE id_cliente = $8
+    WHERE id_cliente = $8
       AND NOT borrado
       AND EXISTS (SELECT 1 FROM planes WHERE id_plan = $5)
       AND EXISTS (SELECT 1 FROM usuarios WHERE id_usuario = $6)
       AND NOT EXISTS (SELECT 1 FROM clientes WHERE correo = $4 AND id_cliente <> $8)
     RETURNING *;
   `;
+
   try {
     const { id_cliente } = req.params;
-    const {
-      nombre, ubicacion, telefono, correo, id_plan, estado_usuario
-    } = req.body;
-
+    const { nombre, ubicacion, telefono, correo, id_plan, estado_usuario } = req.body;
     const operacion = req.method;
     const id_usuario = req.headers['id_usuario'];
 
@@ -98,33 +119,36 @@ routerClientes.put('/:id_cliente', validaClientes, validaidClientes, async (req,
     ]);
 
     if (actualizarCliente.rowCount > 0) {
-      auditar(operacion,id_usuario);
+      auditar(operacion, id_usuario);
       return res.status(200).json({ mensaje: 'Cliente actualizado exitosamente' });
     } else {
-      return res.status(400).json({ error: 'Error al crear el cliente' });
+      return res.status(400).json({ error: 'Error al actualizar el cliente' });
     }
   } catch (error) {
     console.error('Error al actualizar el cliente:', error);
   }
 });
 
-
-
-//borrar cliente
+/**
+ * PATCH /:id_cliente
+ * Marca un cliente como borrado (eliminación lógica).
+ *
+ * @route PATCH /:id_cliente
+ * @param {string} id_cliente - ID del cliente a eliminar.
+ * @returns {Object} - Mensaje de éxito o error.
+ */
 routerClientes.patch('/:id_cliente', validaidClientes, async (req, res) => {
   try {
     const { id_cliente } = req.params;
     const operacion = req.method;
     const id_usuario = req.headers['id_usuario'];
 
-    // Validacion #1
     const clienteExistente = await pool.query('SELECT * FROM clientes WHERE id_cliente = $1 AND borrado = false', [id_cliente]);
 
     if (clienteExistente.rowCount === 0) {
       return res.status(404).json({ error: 'Datos incorrectos' });
     }
 
-    // Actualiza el cliente y marca como borrado
     const updateQuery = `
       UPDATE clientes 
       SET borrado = true
@@ -135,7 +159,7 @@ routerClientes.patch('/:id_cliente', validaidClientes, async (req, res) => {
     await pool.query(updateQuery, [id_cliente]);
 
     res.json({ mensaje: 'Cliente eliminado correctamente' });
-    auditar(operacion,id_usuario);
+    auditar(operacion, id_usuario);
     
   } catch (error) {
     console.error('Error al marcar cliente como borrado:', error);
@@ -143,19 +167,30 @@ routerClientes.patch('/:id_cliente', validaidClientes, async (req, res) => {
   }
 });
 
-
-//obtener cliente
+/**
+ * GET /
+ * Obtiene todos los clientes que no están marcados como borrados.
+ *
+ * @route GET /
+ * @returns {Array} - Un arreglo de objetos que representan los clientes.
+ */
 routerClientes.get('/', async (req, res) => {
   try {
     const clientes = await pool.query('SELECT id_cliente, nombre, ubicacion, telefono, correo, id_plan, id_usuario, estado_usuario FROM clientes WHERE borrado = false ORDER BY id_cliente ASC');
     res.json(clientes.rows);
-
   } catch (error) {
     console.log(error);
   }
 });
 
-//obtener un cliente
+/**
+ * GET /:id_cliente
+ * Obtiene los datos de un cliente específico.
+ *
+ * @route GET /:id_cliente
+ * @param {string} id_cliente - ID del cliente a obtener.
+ * @returns {Object} - Un objeto que representa los datos del cliente.
+ */
 routerClientes.get('/:id_cliente', async (req, res) => {
   try {
     const { id_cliente } = req.params;
@@ -164,10 +199,10 @@ routerClientes.get('/:id_cliente', async (req, res) => {
       return res.status(404).json({ error: 'Datos incorrectos' });
     }
     res.json(clientes.rows[0]);
-
   } catch (error) {
     console.log(error);
   }
 });
  
+// Exporta el enrutador para su uso en otras partes de la aplicación
 module.exports = routerClientes;
