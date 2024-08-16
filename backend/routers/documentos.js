2670467d80e6d218418fdecfb999387b325abab0
fixbala <smartineza_2@uqvirtual.edu.co>
2024-08-16 14:11:55 -0500

    Actualizacion de la  documentacion.

--- backend/routers/documentos.js
+++ backend/routers/documentos.js
@@ -1,65 +1,65 @@
-// Importacion de express
+// Importacion de módulos y dependencias
 const express = require('express');
 const cors = require('cors');
 const pool = require('../database/db.js');
 const fs = require('fs');
-const { validationResult } = require('express-validator')
+const { validationResult } = require('express-validator');
 const { join } = require('path');
 const path = require('path');
- 
+
+// Configuración del enrutador para documentos
 const routerDocumentos = express.Router();
 routerDocumentos.use(express.json());
 routerDocumentos.use(cors());
-const { auditar, convertirMayusculas, errorHandler, obtenerFechayHora } = require('../funciones/funciones.js')
-const { validarIdDocumento, validarDocumento, validarActDocumento} = require('../validaciones/ValidarDocumentos.js')
 
-//Crear Documento
+const { auditar, convertirMayusculas, errorHandler, obtenerFechayHora } = require('../funciones/funciones.js');
+const { validarIdDocumento, validarDocumento, validarActDocumento } = require('../validaciones/ValidarDocumentos.js');
 
+// Middleware para cargar documentos
 const { CargaDocumento, CURRENT_DIR } = require('../middleware/DocumentosMulter.js');
 
+// Ruta para crear un nuevo documento
 routerDocumentos.post('/', CargaDocumento.single('documento'), validarDocumento, async (req, res) => {
-  
   const consulta = `
-  INSERT INTO documentos (titulo, descripcion, id_usuario, hora_subida, fecha_subida, permiso, borrado) 
-  VALUES ($1, $2, 1, CAST($3 as time), CAST($4 as date), $5, false) RETURNING *;
-`;
+    INSERT INTO documentos (titulo, descripcion, id_usuario, hora_subida, fecha_subida, permiso, borrado) 
+    VALUES ($1, $2, 1, CAST($3 as time), CAST($4 as date), $5, false) RETURNING *;
+  `;
 
-try {
-  const { titulo, descripcion, hora_subida, fecha_subida, permiso } = req.body;
-  const operacion = req.method;
-  const id_usuarioAuditoria = req.headers['id_usuario'];
-  const documento = req.file.filename;
+  try {
+    const { titulo, descripcion, hora_subida, fecha_subida, permiso } = req.body;
+    const operacion = req.method;
+    const id_usuarioAuditoria = req.headers['id_usuario'];
+    const documento = req.file.filename;
 
-  // Construir una cadena de fecha y hora válida
-  const fechaActual = obtenerFechayHora("fecha");
-  const horaActual = obtenerFechayHora("hora");
+    // Obtener la fecha y hora actual
+    const fechaActual = obtenerFechayHora("fecha");
+    const horaActual = obtenerFechayHora("hora");
 
-  const errores = validationResult(req);
+    // Validar errores
+    const errores = validationResult(req);
+    if (errores.isEmpty()) {
+      const crearDocumento = await pool.query(consulta, [
+        documento, descripcion, horaActual, fechaActual, permiso
+      ]);
 
-  if (errores.isEmpty()) {
-    const crearDocumento = await pool.query(consulta, [
-      documento, descripcion, horaActual, fechaActual, permiso
-    ]);
-    if (crearDocumento.rows.length > 0) {
-      auditar(operacion, id_usuarioAuditoria);
-      return res.status(200).json({ mensaje: 'Documento creado exitosamente' });
-    }else {
+      if (crearDocumento.rows.length > 0) {
+        auditar(operacion, id_usuarioAuditoria);
+        return res.status(200).json({ mensaje: 'Documento creado exitosamente' });
+      } else {
+        fs.unlinkSync(req.file.path);
+        return res.status(400).json({ error: 'Error al crear el documento' });
+      }
+    } else {
       fs.unlinkSync(req.file.path);
-      return res.status(400).json({ error: 'Error al crear el documento' });
+      return res.status(400).json({ error: 'Datos incorrectos' });
     }
-  } else {
-    fs.unlinkSync(req.file.path);
-    return res.status(400).json({ error: 'Datos incorrectos' });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(400).json({ error: 'Error al crear el documento' });
   }
-} catch (error) {
-  console.error(error.message);
-  return res.status(400).json({ error: 'Error al crear el documento' });
-}
-
 });
 
-
-//Modificar Documentos---------------------------------------------------------------------------
+// Ruta para modificar un documento existente usando PUT
 routerDocumentos.put('/:id_documento', CargaDocumento.single('documento'), validarIdDocumento, validarActDocumento, validarDocumento, async (req, res) => {
   const { id_documento } = req.params;
   const { descripcion, fecha_subida, hora_subida, permiso } = req.body;
@@ -73,25 +73,23 @@ routerDocumentos.put('/:id_documento', CargaDocumento.single('documento'), valid
 
   try {
     const errores = validationResult(req);
-
     if (errores.isEmpty()) {
-      // Query SQL para actualizar el documento con validación de existencia
       const query = `
-      UPDATE documentos
-      SET
-        titulo = $1,
-        descripcion = $2,
-        permiso = $3,
-        fecha_subida = $4, 
-        hora_subida = $5 
-      WHERE id_documento = $6
-        AND NOT borrado
-        AND EXISTS (SELECT 1 FROM documentos WHERE id_documento = $6)
-      RETURNING *;
+        UPDATE documentos
+        SET
+          titulo = $1,
+          descripcion = $2,
+          permiso = $3,
+          fecha_subida = $4, 
+          hora_subida = $5 
+        WHERE id_documento = $6
+          AND NOT borrado
+          AND EXISTS (SELECT 1 FROM documentos WHERE id_documento = $6)
+        RETURNING *;
       `;
 
       const fechaActual = obtenerFechayHora("fecha");
-      const horaActual = obtenerFechayHora("hora");;
+      const horaActual = obtenerFechayHora("hora");
 
       const values = [
         documento || nombreDocumentoActual, 
@@ -105,9 +103,9 @@ routerDocumentos.put('/:id_documento', CargaDocumento.single('documento'), valid
       const actualizarDocumento = await pool.query(query, values);
 
       if (actualizarDocumento.rowCount > 0) {
-        if (documento && nombreDocumentoActual ) {
+        if (documento && nombreDocumentoActual) {
           const pathDocAnterior = join(__dirname, '../cargas', nombreDocumentoActual);
-  
+
           fs.unlink(pathDocAnterior, (err) => {
             if (err) {
               console.error('Error al eliminar documento anterior:', err);
@@ -116,12 +114,12 @@ routerDocumentos.put('/:id_documento', CargaDocumento.single('documento'), valid
             }
           });
         }
-  
+
         return res.status(200).json({ mensaje: 'Documento actualizado exitosamente' });
       } else {
         if (documento) {
           const pathDocNuevo = join(__dirname, '../cargas', documento);
-      
+
           fs.unlink(pathDocNuevo, (err) => {
             if (err) {
               console.error('Error al eliminar el documento nuevo:', err);
@@ -130,13 +128,12 @@ routerDocumentos.put('/:id_documento', CargaDocumento.single('documento'), valid
             }
           });
         }
-        
+
         return res.status(400).json({ error: 'Error al actualizar el documento' });
       }
     } else {
-      
       fs.unlinkSync(req.file.path);
-      return res.status(400).json({ error: 'Datos incorrectos'});
+      return res.status(400).json({ error: 'Datos incorrectos' });
     }
   } catch (error) {
     console.error('Error al actualizar el documento:', error);
@@ -144,16 +141,14 @@ routerDocumentos.put('/:id_documento', CargaDocumento.single('documento'), valid
   }
 });
 
-// modificar documento con patch
+// Ruta para modificar parcialmente un documento usando PATCH
 routerDocumentos.patch('/edit/:id_documento', validarIdDocumento, validarActDocumento, validarDocumento, async (req, res) => {
   const { id_documento } = req.params;
   const { titulo, descripcion, fecha_subida, hora_subida, permiso } = req.body;
 
   try {
     const errores = validationResult(req);
-
     if (errores.isEmpty()) {
-      // Obtener el nombre actual del documento en la base de datos
       const queryNombreDocumento = 'SELECT titulo FROM documentos WHERE id_documento = $1';
       const resultNombreDocumento = await pool.query(queryNombreDocumento, [id_documento]);
       const nombreDocumentoActual = resultNombreDocumento.rows.length > 0 ? resultNombreDocumento.rows[0].titulo : null;
@@ -162,18 +157,15 @@ routerDocumentos.patch('/edit/:id_documento', validarIdDocumento, validarActDocu
         return res.status(404).json({ error: 'Documento no encontrado' });
       }
 
-      // Si el título actual es igual al nuevo título, no realiza cambios en el nombre del archivo
       let nuevoNombreDocumento = nombreDocumentoActual;
       if (nombreDocumentoActual !== titulo) {
         const extDocumento = path.extname(nombreDocumentoActual);
         nuevoNombreDocumento = `${titulo}-${Date.now()}${extDocumento}`;
-        // Renombrar el archivo en la carpeta cargas
         const pathDocumentoActual = path.join(__dirname, '../cargas', nombreDocumentoActual);
         const pathNuevoDocumento = path.join(__dirname, '../cargas', nuevoNombreDocumento);
         fs.renameSync(pathDocumentoActual, pathNuevoDocumento);
       }
 
-      // Query SQL para actualizar el documento
       const query = `
         UPDATE documentos
         SET
@@ -216,73 +208,34 @@ routerDocumentos.patch('/edit/:id_documento', validarIdDocumento, validarActDocu
   }
 });
 
-
-
-routerDocumentos.use(errorHandler);
-  
-  //Obtener un Documento
-  
+// Ruta para obtener un documento específico
 routerDocumentos.get('/:id_documento', validarIdDocumento, validarDocumento, async (req, res) => {
   try {
     const { id_documento } = req.params;
-    const documentos = await pool.query('SELECT id_documento, titulo, descripcion, id_usuario, hora_subida, fecha_subida FROM documentos WHERE (id_documento = $1) AND borrado = false ', [id_documento]);
-    console.log(id_documento)
+    const documentos = await pool.query('SELECT id_documento, titulo, descripcion, id_usuario, hora_subida, fecha_subida FROM documentos WHERE (id_documento = $1) AND borrado = false', [id_documento]);
     if (documentos.rowCount === 0) {
       return res.status(404).json({ error: 'Documento no encontrado' });
     }
     res.json(documentos.rows[0]);
-
   } catch (error) {
     console.log(error);
   }
 });
 
-// Obtener Documentos
+// Ruta para obtener todos los documentos
 routerDocumentos.get('/', async (req, res) => {
   try {
     const documentos = await pool.query('SELECT id_documento, titulo, descripcion, id_usuario, hora_subida, fecha_subida, permiso FROM documentos WHERE borrado = false ORDER BY id_documento ASC');
-    
+
     const documentosFormateados = documentos.rows.map((documento) => ({
       ...documento,
       fecha_subida: documento.fecha_subida.toISOString().split('T')[0], // Formatea a "AAAA-MM-DD"
     }));
-    
+
     res.json(documentosFormateados);
   } catch (error) {
     console.log(error);
   }
-});
+}); 
 
-
-
-  // Eliminar Documento
-routerDocumentos.patch('/:id_documento', validarIdDocumento, validarDocumento, async (req, res) => {
-  try {
-    const { id_documento } = req.params;
-
-    // Validacion #1
-    const documentoexiste = await pool.query('SELECT * FROM documentos WHERE id_documento = $1 AND borrado = false', [id_documento]);
-
-    if (documentoexiste.rowCount === 0) {
-      return res.status(404).json({ error: 'Documento no encontrado' });
-    }
-
-    // Actualiza el documento y marca como borrado
-    const updateQuery = `
-      UPDATE documentos 
-      SET borrado = true
-      WHERE id_documento = $1
-      RETURNING *;
-    `;
-
-    await pool.query(updateQuery, [id_documento]);
-
-    res.json({ mensaje: 'documento eliminado correctamente' });
-  } catch (error) {
-    console.error('Error al marcar documento como borrado:', error);
-    res.status(500).json({ error: 'Error al marcar documento como borrado' });
-  }
-});
- 
-
-module.exports = routerDocumentos ;
+module.exports = routerDocumentos;
