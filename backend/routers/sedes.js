811cbe17661455e32c6115914d5ea205f4c285a3
fixbala <smartineza_2@uqvirtual.edu.co>
2024-08-21 20:23:44 -0500

    Inicio de creacion de un nuevo router.

---
+++ backend/routers/sedes.js
@@ -0,0 +1,38 @@
+// Importacion de express
+const express = require('express');
+const cors = require('cors');
+const pool = require('../database/db.js');
+const routerSedes= express.Router();
+
+routerSedes.use(express.json());
+routerSedes.use(cors());
+
+// get all
+routerSedes.get('/', async (req, res) => {
+    try {
+      const result = await pool.query("SELECT nombre_sede,latitud,longitud,ip FROM sedes WHERE borrado is not true order by nombre_sede " )
+      res.json(result.rows);
+    } catch (error) {
+      res.status(500).json({ message: "Ha ocurrido un error" });
+    }
+  });
+
+  //get a sede
+
+routerSedes.get('/:id_sede', async (req, res) => {
+  try {
+    const { id_sede } = req.params;
+    const sedes = await pool.query('SELECT nombre_sede,latitud,longitud,ip FROM sedes WHERE borrado is not true and id_sede = $1', [id_sede]);
+
+    if (sedes.rowCount === 0) {
+      return res.status(404).json({ error: 'La sede no fue encontrada' });
+    }
+    res.json(sedes.rows[0]);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+  
+  module.exports = routerSedes;
+  
