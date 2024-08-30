2a2d44b5d828d110f708b117cfb7734fd2e7fffc
fixbala <smartineza_2@uqvirtual.edu.co>
2024-08-30 09:51:00 -0500

    Continuacion del entorno frontend.

---
+++ frontend/src/components/Panel_Lateral/Panel_Lateral.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import RocketLogo from "../../assets/img/ROCKETNETLOGO.png";
+import RocketLogo2 from "../../assets/img/ROCKETNET-LOGO2.png";
+import "../Panel_Lateral/Panel_Lateral.css";
+
+function Panel_Lateral() {
+  return (
+    <div className="panel-lateral">
+      <img className="logo" src={RocketLogo} alt="Logo RocketNet" />
+      <img className="logo2" src={RocketLogo2} alt="Logo RocketNet 2"/>
+    </div>
+  );
+} 
+
+export default Panel_Lateral;
